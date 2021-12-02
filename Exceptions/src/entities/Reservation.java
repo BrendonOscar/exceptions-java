3dcc91c18198ebab5395b5deab531c9ba7e3413e
Brendon Silva <brendon.oscarsilva@gmail.com>
2021-12-01 23:48:08 -0300

    Exceptions Good Solution

--- Exceptions/src/entities/Reservation.java
+++ Exceptions/src/entities/Reservation.java
@@ -3,6 +3,7 @@ package entities;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import exceptions.ResExceptions;
 
     public class Reservation{
    
@@ -12,7 +13,10 @@ import java.util.concurrent.TimeUnit;
 
         private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws ResExceptions{
+            if (!checkOut.after(checkIn)) {
+				throw new ResExceptions("Check-out date must be after check-in date");
+            }
             this.roomNumber = roomNumber;
             this.checkIn = checkIn;
             this.checkOut = checkOut;
@@ -40,17 +44,17 @@ import java.util.concurrent.TimeUnit;
         }
         //Long type because compatibility
 
-        public String updateDates(Date checkIn, Date checkOut) {
+        public void updateDates(Date checkIn, Date checkOut) throws ResExceptions {
             Date now = new Date();
 			if (checkIn.before(now) || checkOut.before(now)) {
-				return ("Reservation dates for update must be future dates");
+				throw new ResExceptions("Reservation dates for update must be future dates");
 			}
 			if (!checkOut.after(checkIn)) {
-				return ("Check-out date must be after check-in date");
+				throw new ResExceptions("Check-out date must be after check-in date");
 			}
             this.checkIn = checkIn;
             this.checkOut = checkOut;
-            return null;
+            
         }
 
         @Override
---
+++ Exceptions/src/exceptions/ResExceptions.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class ResExceptions extends Exception{
+    
+    
+    public ResExceptions(String msg) {
+        super(msg);
+    }
+}
+//Created a personalized exception
